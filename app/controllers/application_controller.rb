ca35538b8fb6131588417f039269e22cd8603372
Naomi-Rhames <naomirhames2020@gmail.com>
2022-03-14 22:56:57 -0500

    user auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+
+    def render_token(user)
+        render json: { user: ActiveModelSerializers::SerializableResource.new(user, serializer: UserSerializer), token: encode_token(user.id) }
+    end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,11 @@
 class UsersController < ApplicationController
+
+    def signup 
+        user = User.new(user_params)
+        if user.save
+            render_token(user)
+        else
+            render json: {error: user.errors.full_messages},  status: 403
+        end 
+    end
 end
