6bce11909c09eada242fe85227a8ed10dda7529c
Naomi-Rhames <naomirhames2020@gmail.com>
2022-03-21 14:12:11 -0500

    added post controller action/ seed data

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,20 @@
 class PostsController < ApplicationController
     
+    def create
+        post = Post.new(post_params)
+        if post.save
+            render json: post
+        else
+            render json: {error: post.errors.full_messages},  status: 403
+        end 
+    end
+    
+
+
+
+    private
+
+    def post_params
+        params.permit(:message, :image_url, :user_id)
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+naomi = User.create(email: "test101@gmail.com", bio: "A female who loves affirmation", password: "password123")
+
+
+
+first_post = Post.create(message: "I am capable to do the things that are challenging for me", image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMTajPc_CUxpskWZVlQtr7Vxx528Ubm8r3Wg&usqp=CAU", user_id: naomi.id)
+ 
+ 
+# main_user = User.create(email: "me@gmail.com", bio: "Affirmation is all I need", password: "Password1243")
