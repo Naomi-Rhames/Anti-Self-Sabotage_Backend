b792e1cde9a3d76a014cc828824b171d71f5112c
Naomi-Rhames <naomirhames2020@gmail.com>
2022-03-25 13:57:31 -0500

    Applying Jwt token for User Auth

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
     def signup 
         user = User.new(user_params)
         if user.save
-        render json: user
+        render_token(user)
         else
             render json: {error: user.errors.full_messages},  status: 403
         end 
