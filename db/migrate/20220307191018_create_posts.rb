8bb101454a0385224415e3563dfb7757777c0e9f
Naomi-Rhames <naomirhames2020@gmail.com>
2022-08-19 22:40:06 -0500

    deleting unused spaces

--- db/migrate/20220307191018_create_posts.rb
+++ db/migrate/20220307191018_create_posts.rb
@@ -4,7 +4,6 @@ class CreatePosts < ActiveRecord::Migration[6.1]
       t.text :message
       t.string :image_url
       t.integer :user_id
-
       t.timestamps
     end
   end
--- db/migrate/20220323182531_create_affirmations.rb
+++ db/migrate/20220323182531_create_affirmations.rb
@@ -2,7 +2,6 @@ class CreateAffirmations < ActiveRecord::Migration[6.1]
   def change
     create_table :affirmations do |t|
       t.string :phrase
-
       t.timestamps
     end
   end
