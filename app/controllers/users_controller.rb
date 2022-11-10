e25c20b8f73918f1e3c522b54f0ff692edf5b29f
Naomi-Rhames <naomirhames2020@gmail.com>
2022-11-09 22:06:24 -0600

    brew update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,31 +1,31 @@
 class UsersController < ApplicationController
 
-    # def signup 
-    #     user = User.new(user_params)
-    #     if user.save
-    #     render_token(user)
-    #     else
-    #         render json: {error: user.errors.full_messages},  status: 403
-    #     end 
-    # end
+    def signup 
+        user = User.new(user_params)
+        if user.save
+        render_token(user)
+        else
+            render json: {error: user.errors.full_messages},  status: 403
+        end 
+    end
 
-    # def index 
-    #     user = User.all
-    #     render json: user
-    # end
+    def index 
+        user = User.all
+        render json: user
+    end
 
-    # def show
-    #     user = User.find(params[:id])
-    #     if user === fetch_user
-    #        render json: user
-    #     else
-    #         render json: {error: user.errors.full_messages}
-    #     end
-    # end
+    def show
+        user = User.find(params[:id])
+        if user === fetch_user
+           render json: user
+        else
+            render json: {error: user.errors.full_messages}
+        end
+    end
 
-    # private
+    private
 
-    # def user_params
-    #     params.permit(:profile_photo, :email, :username ,:bio, :password)
-    # end
+    def user_params
+        params.permit(:profile_photo, :email, :username ,:bio, :password)
+    end
 end
